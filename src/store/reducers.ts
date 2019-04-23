9b2c1322806a41f78a6de3c096287f55829b093d
Anatoly Pankratov <asceticat@gmail.com>
2019-04-23 21:42:34 +0300

    Added tslint. Fixed lint errors.

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,27 +1,27 @@
 import { combineReducers } from "redux";
 
 export interface ITestReducer {
-  value: number
+  value: number;
 }
 
 const initialState: ITestReducer = {
-  value: 0
+  value: 0,
 };
 
-function testReducer (state: ITestReducer = initialState, action) {
-  switch(action.type) {
-    case "INCREASE": 
+function testReducer(state: ITestReducer = initialState, action) {
+  switch (action.type) {
+    case "INCREASE":
       return {
-        value: state.value + 1
+        value: state.value + 1,
       };
     case "DECREASE":
       return {
-        value: state.value - 1
+        value: state.value - 1,
       };
   }
   return state;
 }
 
 export default combineReducers({
-  testReducer
+  testReducer,
 });
